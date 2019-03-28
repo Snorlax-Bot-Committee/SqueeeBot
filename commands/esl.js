d544673673aba0a0f76f6e399b59a73c4285774f
Jenny Shi <jennpeare@gmail.com>
2019-03-27 21:16:44 -0400

    maybe i should test edge cases more often, also api stop changing return format pls

--- commands/esl.js
+++ commands/esl.js
@@ -9,7 +9,13 @@ const run = (bot, message, args) => {
   const messenger = new Messenger(bot, message);
 
   if (args.length == 1) {
-    define(args[0]).then(embed => messenger.sendText({ embed }));
+    define(args[0]).then(embed => {
+      if (embed) {
+        messenger.sendText({ embed });
+      } else {
+        messenger.sendText('Tsk tsk tsk, making up words now are we?');
+      }
+    });
   } else if (args.length == 0) {
     messenger.sendText('Yes you are');
   } else {
@@ -18,30 +24,42 @@ const run = (bot, message, args) => {
 };
 
 const define = (term) => {
-  return request(`${baseUrl}?lang=en&define=${term}`).then(data => {
-    const word = JSON.parse(data);
+  return request(`${baseUrl}?lang=en&define=${term}`)
+    .then(data => {
+      let word = JSON.parse(data);
 
-    let embed = new Discord.RichEmbed()
-      .setTitle(word.word)
-      .setThumbnail(dictThumb)
-      .setColor('#ffc107');
-
-    if (word.phonetic) {
-      embed.setDescription('`' + word.phonetic + '`');
-    }
-
-    let entries = Object.entries(word.meaning);
-    for (const [pos, entry] of entries) {
-      if (entry.length > 1) {
-        const def = entry.reduce((acc, entry) => `+ ${entry.definition}\n${acc}`, '');
-        embed.addField(pos, def);
-      } else {
-        embed.addField(pos, entry[0]['definition']);
+      // in case api is fickle?
+      if (Array.isArray(word)) {
+        word = word[0];
       }
-    }
 
-    return embed;
-  });
+      let embed = new Discord.RichEmbed()
+        .setTitle(word.word)
+        .setThumbnail(dictThumb)
+        .setColor('#ffc107');
+
+      if (word.phonetic) {
+        embed.setDescription('`' + word.phonetic + '`');
+      }
+
+      let entries = Object.entries(word.meaning);
+      for (const [pos, entry] of entries) {
+        if (entry.length > 1) {
+          const def = entry.reduce((acc, entry) => `+ ${entry.definition}\n${acc}`, '');
+          embed.addField(pos, def);
+        } else {
+          embed.addField(pos, entry[0]['definition']);
+        }
+      }
+
+      return embed;
+    })
+    .catch(err => {
+      // most likely because word doesn't exist and api doesn't 
+      // return an empty [] or {} for whatever reason
+      console.error(err);
+      return null;
+    });
 
 };
 
